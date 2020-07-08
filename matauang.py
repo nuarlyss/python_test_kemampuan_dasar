9733bf69853cb34415c695097b1e28e0ac245953
Rizqi Putri Nourma Budiarti <rizqi.putri.nb@gmail.com>
2020-07-09 02:54:32 +0700

    Create matauang.py

---
+++ matauang.py
@@ -0,0 +1,12 @@
+
+dmatauang2idr = {'IDR': 1, 'USD': 14425, 'EUR': 16225}
+ 
+def usd2eur(usd):
+  
+  USD = dmatauang2idr['USD'] * usd
+  
+  EUR = USD / dmatauang2idr['EUR'] 
+  value = EUR
+  return value
+ 
+print(usd2eur(1000))
